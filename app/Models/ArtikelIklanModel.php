b054fce2a1b4913e5f12c5129ec69dde192cb992
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2025-04-26 12:10:58 +0700

    edit controller

--- app/Models/ArtikelIklanModel.php
+++ app/Models/ArtikelIklanModel.php
@@ -7,9 +7,9 @@ use CodeIgniter\Model;
 class ArtikelIklanModel extends Model
 {
     protected $table = 'tb_artikel_iklan';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id_iklan';
     protected $returnType = 'array';
-    protected $allowedFields = ['id', 'id_artikel', 'id_iklan', 'id_user', 'tanggal_mulai', 'tanggal_selesai', 'status'];
+    protected $allowedFields = ['id', 'id_artikel', 'id_iklan', 'id_penulis', 'tanggal_mulai', 'tanggal_selesai', 'status'];
 
     public function getArtikelIklan()
     {
@@ -22,7 +22,7 @@ class ArtikelIklanModel extends Model
         ')
             ->join('tb_artikel', 'tb_artikel.id_artikel = tb_artikel_iklan.id_artikel')
             ->join('tb_harga_iklan', 'tb_harga_iklan.id = tb_artikel_iklan.id_iklan')
-            ->join('tb_users', 'tb_users.id_user = tb_artikel_iklan.id_user')
+            ->join('tb_users', 'tb_users.id_user = tb_artikel_iklan.id_penulis')
             ->findAll();
     }
 }
