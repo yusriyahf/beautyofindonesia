55d3eb31320fdccd968496554e02a9b785e38624
ichaputri <ichaputriana39@gmail.com>
2025-04-28 12:21:28 +0700

    Navbar : Add Menu Tampil Permintaan Artikel Iklan

--- app/Views/admin/layout/navbar.php
+++ app/Views/admin/layout/navbar.php
@@ -23,7 +23,7 @@
                         <span class="nav-link-text">Dashboard</span>
                     </a><!--//nav-link-->
                 </li><!--//nav-item-->
-                
+
 
 
 
@@ -96,6 +96,19 @@
                                     <span class="nav-link-text">Kategori</span>
                                 </a>
                             </li>
+
+                            <!-- TAMPIL POPUP -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= base_url('admin/artikeliklan') ?>">
+                                    <span class="nav-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-aspect-ratio" viewBox="0 0 16 16">
+                                            <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z" />
+                                            <path d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0z" />
+                                        </svg>
+                                    </span>
+                                    <span class="nav-link-text">Permintaan Tampil Iklan</span>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </li>
@@ -117,6 +130,8 @@
                         <span class="nav-link-text">Tampil Popup</span>
                     </a>
                 </li>
+
+
 
                 <!-- LIST POPUP -->
                 <li class="nav-item">
@@ -262,6 +277,4 @@
     artikelMenu.addEventListener('hidden.bs.collapse', function() {
         arrowIcon.classList.remove('rotate');
     });
-
-    
 </script>
