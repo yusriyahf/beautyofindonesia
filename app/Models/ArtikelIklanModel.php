a629d17e5f148023864cd1a7990642e7230a18d6
ichaputri <ichaputriana39@gmail.com>
2025-04-28 12:19:47 +0700

    ArtikelIklanModel (Admin) : Add Func getArtikelIklan

--- app/Models/ArtikelIklanModel.php
+++ app/Models/ArtikelIklanModel.php
@@ -54,4 +54,19 @@ class ArtikelIklanModel extends Model
         // Menggunakan get() untuk menjalankan query
         return $builder->get()->getResultArray();
     }
+
+    public function getArtikelIklan()
+    {
+        return $this->select('
+            tb_artikel_iklan.*, 
+            tb_artikel.judul_artikel, 
+            tb_harga_iklan.nama AS nama_iklan, 
+            tb_users.username, 
+            tb_users.kontak
+        ')
+            ->join('tb_artikel', 'tb_artikel.id_artikel = tb_artikel_iklan.id_artikel')
+            ->join('tb_harga_iklan', 'tb_harga_iklan.id_harga_iklan = tb_artikel_iklan.id_harga_iklan')
+            ->join('tb_users', 'tb_users.id_user = tb_artikel_iklan.id_penulis')
+            ->findAll();
+    }
 }
